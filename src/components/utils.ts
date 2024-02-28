7cbed41c66aaf671bdd43d7ffc6a6e076947f736
Shubham Sharma <shubham-int@sharefable.com>
2024-02-28 16:35:40 +0530

    refactor

--- src/components/utils.ts
+++ src/components/utils.ts
@@ -1,3 +1,5 @@
+import { JourneyModuleWithAnns } from './types';
+
 export const navAnn = (
   dir: 'prev' | 'next',
   fableRef: React.RefObject<HTMLIFrameElement>
@@ -35,4 +37,46 @@ export const goToFirstAnnOfModule = (
       '*'
     );
   }
+};
+
+export const calculateDiff = (
+  clickedAnnIdx: number,
+  currAnnIdx: number,
+  clickedJourneyIdx: number,
+  currJourneyIdx: number,
+  journeyData: JourneyModuleWithAnns[]
+) => {
+  let diff = 0;
+  if (currJourneyIdx !== clickedJourneyIdx) {
+    const startIdx = Math.min(currJourneyIdx, clickedJourneyIdx);
+    const endIdx = Math.max(currJourneyIdx, clickedJourneyIdx);
+    for (let i = startIdx + 1; i < endIdx; i++) {
+      diff += journeyData[i].annsInOrder.length;
+    }
+    if (clickedJourneyIdx - currJourneyIdx >= 1) {
+      diff += journeyData[currJourneyIdx].annsInOrder.length - currAnnIdx + clickedAnnIdx;
+    } else {
+      diff += clickedAnnIdx - journeyData[clickedJourneyIdx].annsInOrder.length - currAnnIdx;
+    }
+  } else {
+    diff = clickedAnnIdx - currAnnIdx;
+  }
+  return diff;
+};
+
+export const navigate = (
+  direction: 'next' | 'prev',
+  noOfTimes: number,
+  navAnn: (dir: 'prev' | 'next', fableRef: React.RefObject<HTMLIFrameElement>) => void,
+  fableRef: React.RefObject<HTMLIFrameElement>,
+  wait: number = 1500
+) => {
+  let i = 0;
+  const interval = setInterval(() => {
+    if (i >= noOfTimes - 1) {
+      clearInterval(interval);
+    }
+    navAnn(direction, fableRef);
+    i++;
+  }, wait);
 };
--- src/template/FableHoc.tsx
+++ src/template/FableHoc.tsx
@@ -1,15 +1,16 @@
 import React, { useCallback, useState, useRef, useEffect } from 'react';
 import FableEmbed from '../components/FableEmbed';
-import { navAnn, goToFirstAnnOfModule } from '../components/utils';
+import { navAnn, goToFirstAnnOfModule, calculateDiff, navigate } from '../components/utils';
 import { IAnnotationConfig, JourneyModuleWithAnns } from '../components/types';
 
 interface IProps {
-  layout?: 'row' | 'col';
-  origin?: string;
   demoRid: string;
+  layout?: 'sidebyside' | 'stacked';
+  origin?: string;
+  contentWidthPercentage?: number;
 }
 
-const FableHoc = ({ layout = 'col', origin, demoRid, ...rest }: IProps) => {
+const FableHoc = ({ layout = 'sidebyside', origin, demoRid, contentWidthPercentage = 30, ...rest }: IProps) => {
   const [currAnnRefId, setCurrAnnRefId] = useState<string>('');
   const [isAnnLoaded, setIsAnnLoaded] = useState(false);
   const [journeyData, setJourneyData] = useState<JourneyModuleWithAnns[] | null>(null);
@@ -20,64 +21,24 @@ const FableHoc = ({ layout = 'col', origin, demoRid, ...rest }: IProps) => {
   const textContainerRef = useRef<HTMLDivElement>(null);
   const [disableMouse, setMouseDisable] = useState(false);
 
-  const handleAnnotationClick = (idx: number, jIdx: number = 0) => {
-    const journeyPresent = journeyData && journeyData.length > 0;
-    let clickedJourneyIdx;
-    let currJourneyIdx;
-    let currAnnIdx;
-    const clickedAnnIdx = idx;
-    const wait = 1500;
-    let diff: number = 0;
-    if (journeyPresent) {
-      clickedJourneyIdx = jIdx;
-      currJourneyIdx = journeyIndex;
-      currAnnIdx = journeyData[currJourneyIdx].annsInOrder.findIndex((ann) => ann.refId === currAnnRefId);
-      if (currJourneyIdx !== clickedJourneyIdx) {
-        const startIdx = Math.min(currJourneyIdx, clickedJourneyIdx);
-        const endIdx = Math.max(currJourneyIdx, clickedJourneyIdx);
-
-        for (let i = startIdx + 1; i < endIdx; i++) {
-          diff += journeyData[i].annsInOrder.length;
-        }
-
-        if (clickedJourneyIdx - currJourneyIdx >= 1) {
-          diff += journeyData[currJourneyIdx].annsInOrder.length - currAnnIdx + clickedAnnIdx;
-        } else {
-          diff += clickedAnnIdx - journeyData[clickedJourneyIdx].annsInOrder.length - currAnnIdx;
-        }
-      } else {
-        diff = clickedAnnIdx - currAnnIdx;
-      }
-      if (diff === 0) {
-        setMouseDisable(false);
-        return;
-      }
-
-      setMouseDisable(true);
-      if (clickedJourneyIdx === currJourneyIdx && diff < 0) {
-        let i = 0;
-        const interval = setInterval(() => {
-          if (i >= Math.abs(diff) - 1) {
-            setMouseDisable(false);
-            clearInterval(interval);
-          }
-          navAnn('prev', fableRef);
-          i++;
-        }, wait);
-        return;
-      }
-
+  const handleNavigation = (
+    diff: number,
+    clickedJourneyIdx: number | undefined,
+    currJourneyIdx: number | undefined,
+    clickedAnnIdx: number,
+    journeyData: JourneyModuleWithAnns[] | null,
+    wait: number,
+  ) => {
+    if (diff === 0) {
+      setMouseDisable(false);
+      return;
+    }
+    setMouseDisable(true);
+    if (journeyData && currJourneyIdx !== undefined && clickedJourneyIdx !== undefined) {
       if (diff < 0) {
         if (clickedJourneyIdx === currJourneyIdx) {
-          let i = 0;
-          const interval = setInterval(() => {
-            if (i >= Math.abs(diff) - 1) {
-              setMouseDisable(false);
-              clearInterval(interval);
-            }
-            navAnn('prev', fableRef);
-            i++;
-          }, wait);
+          navigate('prev', Math.abs(diff), navAnn, fableRef, wait);
+          setMouseDisable(false);
           return;
         } else {
           const jrny = journeyData[clickedJourneyIdx];
@@ -93,49 +54,39 @@ const FableHoc = ({ layout = 'col', origin, demoRid, ...rest }: IProps) => {
           diff = clickedAnnIdx;
         }
       }
+    }
 
-      if (diff > 0) {
-        let i = 0;
-        const interval = setInterval(() => {
-          if (i >= diff - 1) {
-            setMouseDisable(false);
-            clearInterval(interval);
-          }
-          navAnn('next', fableRef);
-          i++;
-        }, 1500);
-        return;
-      }
+    if (diff < 0) {
+      navigate('prev', Math.abs(diff), navAnn, fableRef, wait);
+      setMouseDisable(false);
+      return;
+    }
+    if (diff > 0) {
+      navigate('next', Math.abs(diff), navAnn, fableRef, wait);
+      setMouseDisable(false);
+      return;
+    }
+  };
+
+  const handleAnnotationClick = (idx: number, jIdx: number = 0) => {
+    const journeyPresent = journeyData && journeyData.length > 0;
+    let clickedJourneyIdx;
+    let currJourneyIdx;
+    let currAnnIdx;
+    const clickedAnnIdx = idx;
+    const wait = 1500;
+    let diff: number = 0;
+    if (journeyPresent) {
+      clickedJourneyIdx = jIdx;
+      currJourneyIdx = journeyIndex;
+      currAnnIdx = journeyData[currJourneyIdx].annsInOrder.findIndex((ann) => ann.refId === currAnnRefId);
+      diff = calculateDiff(clickedAnnIdx, currAnnIdx, clickedJourneyIdx, currJourneyIdx, journeyData);
+
     } else {
       currAnnIdx = annConfigs.findIndex((ann) => ann.refId === currAnnRefId);
       diff = clickedAnnIdx - currAnnIdx;
-      if (diff === 0) return;
-      setMouseDisable(true);
-      if (diff < 0) {
-        let i = 0;
-        const interval = setInterval(() => {
-          if (i >= Math.abs(diff) - 1) {
-            setMouseDisable(false);
-            clearInterval(interval);
-          }
-          navAnn('prev', fableRef);
-          i++;
-        }, wait);
-        return;
-      }
-      if (diff > 0) {
-        let i = 0;
-        const interval = setInterval(() => {
-          if (i >= diff - 1) {
-            setMouseDisable(false);
-            clearInterval(interval);
-          }
-          navAnn('next', fableRef);
-          i++;
-        }, wait);
-        return;
-      }
     }
+    handleNavigation(diff, clickedJourneyIdx, currJourneyIdx, clickedAnnIdx, journeyData, wait);
   };
 
   const onLoaded = useCallback((
@@ -145,8 +96,8 @@ const FableHoc = ({ layout = 'col', origin, demoRid, ...rest }: IProps) => {
     demoRid: string,
     jrnyData: JourneyModuleWithAnns[] | null,
   ) => {
-    console.log('onLoaded', annConfigsArr, demoUrl, demoDisplayName, demoRid, jrnyData);
     setIsAnnLoaded(true);
+    console.log('Loaded...', annConfigsArr, demoUrl, demoDisplayName, demoRid, jrnyData);
     if (annConfigsArr && annConfigsArr.length) {
       setAnnConfigs(annConfigsArr);
       setCurrAnnRefId(annConfigsArr[0].refId);
@@ -170,7 +121,7 @@ const FableHoc = ({ layout = 'col', origin, demoRid, ...rest }: IProps) => {
     if (!isAnnLoaded) return;
     const target = textContainerRef.current?.querySelector(`[data-f-id="${currAnnRefId}"]`) as HTMLElement;
     if (!target) return;
-    if (layout === 'row') {
+    if (layout === 'sidebyside') {
       const center = target.offsetTop - window.innerHeight / 2;
       window.scrollTo({
         top: center,
@@ -222,8 +173,8 @@ const FableHoc = ({ layout = 'col', origin, demoRid, ...rest }: IProps) => {
         className=""
         style={{
           display: 'grid',
-          gridTemplateColumns: layout === 'col' ? '1fr' : '340px 1fr',
-          gridTemplateRows: layout === 'col' ? '40rem 1fr' : '1fr',
+          gridTemplateColumns: layout === 'stacked' ? '1fr' : `${contentWidthPercentage}% auto`,
+          gridTemplateRows: layout === 'stacked' ? 'auto 1fr' : '1fr',
           gap: '1rem',
           width: '100%',
           position: 'relative',
@@ -232,9 +183,9 @@ const FableHoc = ({ layout = 'col', origin, demoRid, ...rest }: IProps) => {
           ref={fableConRef}
           style={{
             width: '100%',
-            height: layout === 'col' ? 'auto' : 'fit-content',
-            position: layout === 'row' ? 'sticky' : 'sticky',
-            top: layout === 'row' ? '1.5rem' : '0',
+            height: 'fit-content',
+            position: 'sticky',
+            top: layout === 'sidebyside' ? '1.5rem' : '0',
             zIndex: 1,
           }}
         >
@@ -245,14 +196,16 @@ const FableHoc = ({ layout = 'col', origin, demoRid, ...rest }: IProps) => {
             innerRef={fableRef}
             onAnnotationChange={onChange}
             style={{
-              minHeight: layout === 'row' ? '40rem' : '100%',
+              minHeight: layout === 'sidebyside' ? '40rem' : '30rem',
+              marginTop: '0',
             }}
           />
           <div
             style={{
               display: 'flex',
-              justifyContent: 'space-between',
+              justifyContent: 'end',
               alignItems: 'center',
+              gap: '2rem',
             }}
           >
             <span
@@ -282,17 +235,15 @@ const FableHoc = ({ layout = 'col', origin, demoRid, ...rest }: IProps) => {
           style={{
             margin: '1rem auto',
             cursor: disableMouse ? 'progress' : 'pointer',
-            marginTop: layout === 'col' ? '4rem' : '2rem',
             display: 'flex',
             flexDirection: 'column',
             height: '100%',
-            width: '100%',
-            maxWidth: layout === 'col' ? '40rem' : '20rem',
             scrollbarWidth: 'thin',
             maxHeight: '100%',
-            position: layout === 'col' ? 'sticky' : 'static',
-            top: layout === 'col' ? '1.5rem' : 'auto',
-            order: layout === 'row' ? -1 : 0,
+            position: layout === 'stacked' ? 'sticky' : 'static',
+            top: layout === 'stacked' ? '1.5rem' : 'auto',
+            order: layout === 'sidebyside' ? -1 : 0,
+            width: layout === 'stacked' ? contentWidthPercentage + '%' : 'auto',
           }}
         >
           {journeyData && journeyData?.map((journey, jIdx) => {
